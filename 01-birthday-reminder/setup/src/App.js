9e6b2636ebec923c1e68f7724adf047e3a51976b
AlienQbw <wladyka.bj@gmail.com>
2021-06-30 11:09:20 +0200

    finished birthday project

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,27 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const clearAll = () => {
+    setPeople([]);
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} have birthday today!</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            clearAll();
+          }}
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
