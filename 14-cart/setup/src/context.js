5cb54d0bf469232db227838d86e587bbcdb07b8e
AlienQbw <wladyka.bj@gmail.com>
2021-07-27 02:43:38 +0200

    updated 14 cart project | remove item, clear cart, increase, decrease amount

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -6,13 +6,35 @@ import reducer from './reducer';
 const url = 'https://course-api.com/react-useReducer-cart-project';
 const AppContext = React.createContext();
 
-const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems);
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
 
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const clearCart = () => {
+    dispatch({ type: 'CLEAR_CART' });
+  };
+  const remove = (id) => {
+    dispatch({ type: 'REMOVE', payload: id });
+  };
+  const increase = (id) => {
+    dispatch({ type: 'INCREASE', payload: id });
+  };
+  const deacrease = (id) => {
+    dispatch({ type: 'DEACRESE', payload: id });
+  };
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        deacrease,
       }}
     >
       {children}
