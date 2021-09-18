c531318f26e0db0921bda93c4b6bd1985cc67a44
AlienQbw <wladyka.bj@gmail.com>
2021-09-18 23:40:01 +0200

    intro to 15-cocktails

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quos aperiam
+        ab temporibus unde, enim voluptatem quidem tempora harum nobis vel.
+        Ipsum voluptate laborum debitis dolores quos doloribus aspernatur
+        nesciunt!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
